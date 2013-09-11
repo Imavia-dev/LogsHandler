9970e2799cd90cb2069cf23b0a1e364d338e8953
jerome <jerome.varini@imavia.fr>
2013-09-11 09:22:05 +0200

    Mise en place d'une methodede vérification des arguments

--- logsHandler/Program.cs
+++ logsHandler/Program.cs
@@ -10,11 +10,29 @@ namespace logsHandler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("kljljkljl");
-            Console.WriteLine("kdfjklfjllsds");
-            Console.WriteLine("fkjdklfdl");
-            Console.WriteLine("blabla");
-            Console.WriteLine("blablaV2.0");
+
+    
+                
+
         }
+
+
+        private void checkArguments(string[] args)
+        {
+            // Verification du nombre d'arguments
+            if (args.Length < 3)
+            {
+                Console.WriteLine("logsHandler prise en main rapide");
+                Console.WriteLine("Pour activer le support de requette http utilisez l'option http:true|false");
+                Console.WriteLine("Pour définir le contexte http utilisez l'option httpcontext:{Nom du contexte}");
+                Console.WriteLine("Pour activer la surveillance d'un répertoire de log utilisez l'option watch:{Chemin du dossier}");
+            }
+
+            // Traitement des arguments de la ligne de commande
+
+            // on esaie de decouper l'argument 
+    
+        }
+
     }
 }
